62e55e12bae731797f2205d9dfecac3f61f33f7f
Nemiraj <59046951+Nemiraj@users.noreply.github.com>
2024-07-04 23:41:59 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build', // Specify the output directory here
+  },
+});
